7d73fb3bd98a3c3fa68402d3b5e853a3fcd19299
asi0723 <202380159@psu.palawan.edu.ph>
2026-03-17 01:44:46 +0800

    create app and models

---
+++ maritime/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import ContactMessage
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'sent_at')
+    list_filter = ('sent_at',)
+    search_fields = ('name', 'email', 'message')
+    readonly_fields = ('sent_at',)
